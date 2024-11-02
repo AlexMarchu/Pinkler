db3b8652f736cec704305a70bdc1a924b4691331
AlexMarchu <150007881+AlexMarchu@users.noreply.github.com>
2024-11-03 01:17:03 +1000

    add default avatar image for new users

--- pinkler/urls.py
+++ pinkler/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from .views import landing_view
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('accounts/', include('users.urls')),
     path('feed/', include('feed.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
