2dd58bd1ceb822a838e661435882244f0a6ba3f5
wyborn666 <29ser@list.ru>
2024-11-04 23:28:55 +1000

    Created `UserThemePreferenceAdmin` class to customize admin interface for theme preferences.

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import PinklerUser
+from .models import PinklerUser, UserThemePreference
+
 
 
 class PinklerUserAdmin(UserAdmin):
@@ -15,5 +16,10 @@ class PinklerUserAdmin(UserAdmin):
         (None, {'fields': ('phone_number', 'sex', 'status', 'birthday', 'avatar',)}),
     )
 
+class UserThemePreferenceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'theme', 'font_size', 'primary_color')
 
+admin.site.register(UserThemePreference, UserThemePreferenceAdmin)
 admin.site.register(PinklerUser, PinklerUserAdmin)
+
+
